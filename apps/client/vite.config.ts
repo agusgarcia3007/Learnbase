530c4bec1523d7f8a88ff15be3a3d6d40226065b
agustin garcia <agusgarcia3007@gmail.com>
2025-12-06 22:59:40 -0300

    fix: server nitro in tanstack start

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,12 +3,8 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [
-    tsConfigPaths(),
-    tanstackStart(),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [tsConfigPaths(), tanstackStart(), nitro(), react(), tailwindcss()],
 });
