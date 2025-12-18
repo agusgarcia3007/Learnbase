302f1e8f3eb3c887d69c2216d5b3169990f4bf27
agustin garcia <agusgarcia3007@gmail.com>
2025-12-17 21:06:27 -0300

    fix: poc

--- apps/next/src/app/api/route.ts
+++ apps/next/src/app/api/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
-const API_URL = process.env.NEXT_API_URL;
-export async function GET(request: NextRequest, response: NextResponse) {
+import { NextResponse } from "next/server";
+
+export async function GET() {
   const resp = await fetch(process.env.NEXT_API_URL as string);
   const data: { message: string; version: string } = await resp.json();
   return NextResponse.json(data);
